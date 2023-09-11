5ecfac3bcc1881c51f5470d5e5b382cde13f137b
taifalrubeaan <too1423too@gmail.com>
2023-09-11 12:00:53 +0300

    lab3 with no error

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,9 +14,19 @@ public class MainActivity extends AppCompatActivity {
     EditText send_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        send_button = (Button) findViewById(R.id.send_button_id);
+        send_text = (EditText) findViewById(R.id.send_text_id);
+        send_button.setOnClickListener( this::onClick);
+    }
 
+    private void onClick(View view){
+        String str = send_text.getText().toString();
+        Intent intent =new Intent(getApplicationContext(), MainActivity2.class);
+        intent.putExtra("message_key" , str);
+        startActivity(intent);
+        //* receiver_msg.setText("Hello  "+ str);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,13 +2,19 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    TextView receiver_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        receiver_msg = (TextView) findViewById(R.id.received_value_id);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        receiver_msg.setText("Hello  "+ str +"!");
     }
 }
